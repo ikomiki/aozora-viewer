5f2fec21968dce4764030e50558bf0480f0e9b44
Takuya Matsuda <ikomiki@gmail.com>
2025-09-02 00:18:29 +0900

    デプロイ先の変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/aozora-viewer/' : '/',
   server: {
     host: true,
-    port: 5173
+    port: 5173,
   },
   build: {
     outDir: 'dist',
@@ -17,23 +17,18 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          utils: []
-        }
-      }
+          utils: [],
+        },
+      },
     },
     chunkSizeWarningLimit: 1000,
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
   },
   optimizeDeps: {
-    include: ['react', 'react-dom']
+    include: ['react', 'react-dom'],
   },
   esbuild: {
-    target: 'es2018'
+    target: 'es2018',
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts'
-  }
-})
+});
